988280bf21d7d452f31dd4c1751c373d138051dc
SickRickX2 <riccardo.lupo2611@gmail.com>
2025-01-24 12:53:17 +0100

    CREATE DECK AND SHUFFLE DECK

--- src/model/DeckModel.java
+++ src/model/DeckModel.java
@@ -1,14 +1,28 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DeckModel {
     ArrayList<CardModel> deck = new ArrayList<CardModel>();
+    Random random = new Random();
     public void createDeck(){
         for(Suit suit : Suit.values()){
             for(Value value : Value.values()){
                 deck.add(new CardModel(suit, value));
             }
         }
+    }
+    public void shuffleDeck(){
+        for (int i = 0; 1 < deck.size(); i++){
+            int randomIndex = random.nextInt(deck.size());
+            CardModel tempCard = deck.get(i);
+            CardModel randomCard = deck.get(randomIndex);
+            deck.set(i, randomCard);
+            deck.set(randomIndex, tempCard);
+
+
+        }
+
     }
 }
